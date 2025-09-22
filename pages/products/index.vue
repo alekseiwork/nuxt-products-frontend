212b660d1f3f36201ddd615fd2342acc5b75479b
unknown <moisei118@gmail.com>
2025-09-22 23:29:30 +0200

    add clear table

--- pages/products/index.vue
+++ pages/products/index.vue
@@ -3,6 +3,14 @@ import ProductTable from "~/components/ProductTable.vue";
 import { useProducts } from "~/composables/useProducts";
 
 const { filteredProducts, loading, error, refresh } = useProducts();
+const config = useRuntimeConfig();
+const base = config.public.apiBase || "http://localhost:3001";
+
+const clearProducts = async () => {
+  await fetch(`${base}/products/clear`, {
+    method: "DELETE",
+  });
+};
 </script>
 
 <template>
@@ -10,6 +18,9 @@ const { filteredProducts, loading, error, refresh } = useProducts();
     <div class="flex items-center justify-between mb-6">
       <h1 class="text-2xl font-bold">Список продуктов</h1>
       <div class="flex gap-2">
+        <UButton variant="outline" :loading="loading" @click="clearProducts"
+          >Очистить таблицу</UButton
+        >
         <UButton variant="outline" :loading="loading" @click="refresh"
           >Обновить</UButton
         >
